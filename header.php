debe7d79e19123876ac95bd24948fa2b90b0f153
Vladimir Bykov <vladimir-bykov-1999@mail.ru>
2022-02-16 20:41:58 +0300

    Написана форма авторизации

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" type="text/css" href="css/styles.css">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="/js/script.js"></script>
+        <script type="text/javascript" src="/js/script.js"></script>
     </head>
     <body>
  
